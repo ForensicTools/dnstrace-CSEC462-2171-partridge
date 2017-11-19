1785240318adec6eaeae21452bbdfd35a2bd6eb4
Chris Partridge <root@co3xist.net>
2017-11-19 16:56:18 -0500

    Secured input, testing lookup

--- web/graph1.php
+++ web/graph1.php
@@ -7,8 +7,15 @@ include "inc/setup.php";
 use LayerShifter\TLDExtract\Extract;
 
 $ext = new Extract(null, null, Extract::MODE_ALLOW_ICANN);
-$lookupFQDN = $ext->parse(htmlspecialchars($_GET["domain"]));
+$lookupFQDN = $ext->parse(mysqli_real_escape_string($mysqli, htmlspecialchars($_GET["domain"])));
 
-var_dump($lookupFQDN);
+if(!$lookupFQDN->isValidDomain()) {
+	echo json_encode(array("success" => false, "reason" => "Invalid domain"));
+	exit();
+}
 
-//$doesExistPls = $mysqli->query("SELECT * FROM `Reputation` WHERE ")
+$doesExistPls = $mysqli->query("SELECT * FROM `Reputation` WHERE `Domain` = '".$parsedFQDN->getRegistrableDomain()."' AND `Subdomain` = '".$parsedFQDN["subdomain"]."'");
+
+while($row = $allDomains->fetch_assoc()) {
+	echo $row["Subdomain"] . "." . $row["Domain"] . " found " . PHP_EOL;
+}
