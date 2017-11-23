d1fbf23669775d2c4a38ecac844a876e85bf6936
Chris Partridge <root@co3xist.net>
2017-11-23 15:40:07 -0500

    using CONCAT in MX

--- web/force-directed-domain.php
+++ web/force-directed-domain.php
@@ -146,7 +146,7 @@ while($row = $dbGet->fetch_assoc()) {
 $reducer = array_unique($reducer);
 
 foreach($reducer as $MXD) {
-	$dbGet = $mysqli->query("SELECT * FROM `DNS_MX` WHERE `MX_Subdomain` + '.' + `MX_Domain` = '" . $MXD . "' AND `Domain` != '" . $lookupFQDN->getRegistrableDomain() . "'");
+	$dbGet = $mysqli->query("SELECT * FROM `DNS_MX` WHERE CONCAT(`MX_Subdomain`, '.', `MX_Domain`) = '" . $MXD . "' AND `Domain` != '" . $lookupFQDN->getRegistrableDomain() . "'");
 	
 	while($row = $dbGet->fetch_assoc()) {
 		$preNodes[] = fixDomain($row["Subdomain"], $row["Domain"]);
