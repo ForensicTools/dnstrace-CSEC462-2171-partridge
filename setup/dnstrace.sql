1701d2fc3635773b01abbdab743719647055e312
Chris Partridge <root@co3xist.net>
2017-11-25 20:28:12 -0500

    Database structure changes with complete rework

--- setup/dnstrace.sql
+++ setup/dnstrace.sql
@@ -13,6 +13,13 @@
 CREATE DATABASE IF NOT EXISTS `dnstrace` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `dnstrace`;
 
+-- Dumping structure for table dnstrace.API_Keys
+CREATE TABLE IF NOT EXISTS `API_Keys` (
+  `Key` varchar(256) DEFAULT NULL,
+  `RegisteredTo` varchar(1024) DEFAULT NULL,
+  KEY `Key` (`Key`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- Dumping structure for table dnstrace.DNS_A
 CREATE TABLE IF NOT EXISTS `DNS_A` (
   `Subdomain` varchar(255) DEFAULT NULL,
@@ -44,7 +51,7 @@ CREATE TABLE IF NOT EXISTS `DNS_CNAME` (
   UNIQUE KEY `UNIQ_DATA` (`Subdomain`,`Domain`,`CNAME`),
   KEY `Domain` (`Domain`),
   CONSTRAINT `DNS_CNAME_ibfk_1` FOREIGN KEY (`Domain`) REFERENCES `Reputation` (`Domain`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC COMMENT='type 28';
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC COMMENT='type 5';
 
 -- Dumping structure for table dnstrace.DNS_MX
 CREATE TABLE IF NOT EXISTS `DNS_MX` (
@@ -56,7 +63,7 @@ CREATE TABLE IF NOT EXISTS `DNS_MX` (
   UNIQUE KEY `UNIQ_DATA` (`Subdomain`,`Domain`,`MX_Subdomain`,`MX_Domain`),
   KEY `Domain` (`Domain`),
   CONSTRAINT `DNS_MX_ibfk_1` FOREIGN KEY (`Domain`) REFERENCES `Reputation` (`Domain`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC COMMENT='type 2';
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC COMMENT='type 15';
 
 -- Dumping structure for table dnstrace.DNS_NS
 CREATE TABLE IF NOT EXISTS `DNS_NS` (
@@ -70,11 +77,32 @@ CREATE TABLE IF NOT EXISTS `DNS_NS` (
   CONSTRAINT `DNS_NS_ibfk_1` FOREIGN KEY (`Domain`) REFERENCES `Reputation` (`Domain`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC COMMENT='type 2';
 
+-- Dumping structure for table dnstrace.Jobs
+CREATE TABLE IF NOT EXISTS `Jobs` (
+  `JobID` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `Key` varchar(256) NOT NULL DEFAULT '0',
+  `TimeStart` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `TimeEnd` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `Domain` varchar(300) DEFAULT NULL,
+  `Degree` tinyint(3) unsigned DEFAULT NULL,
+  `MXEN` bit(1) DEFAULT NULL,
+  `NSEN` bit(1) DEFAULT NULL,
+  `Current` varchar(256) DEFAULT 'WAITING',
+  KEY `JobID` (`JobID`),
+  KEY `Current` (`Current`)
+) ENGINE=MEMORY AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+-- Dumping structure for table dnstrace.Processors
+CREATE TABLE IF NOT EXISTS `Processors` (
+  `Count` int(11) DEFAULT NULL
+) ENGINE=MEMORY DEFAULT CHARSET=latin1 ROW_FORMAT=FIXED;
+
 -- Dumping structure for table dnstrace.Reputation
 CREATE TABLE IF NOT EXISTS `Reputation` (
   `Subdomain` varchar(255) DEFAULT NULL,
   `Domain` varchar(255) DEFAULT NULL,
   `Source` varchar(255) DEFAULT NULL,
+  UNIQUE KEY `KEEP_DATA_SEMIUNIQ` (`Subdomain`,`Domain`,`Source`),
   KEY `ReputationDomain` (`Domain`),
   KEY `ReputationSource` (`Source`),
   CONSTRAINT `REP-SRC` FOREIGN KEY (`Source`) REFERENCES `Sources` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -124,6 +152,9 @@ CREATE TABLE IF NOT EXISTS `WHOIS_General` (
 CREATE TABLE IF NOT EXISTS `Worker` (
   `Count` int(11) DEFAULT NULL
 ) ENGINE=MEMORY DEFAULT CHARSET=latin1;
+
+INSERT IGNORE INTO `Sources` (`ID`, `Description`, `Score`) VALUES
+	('USERADD', 'Subdomain was added manually, and has no scoring weight.', 0);
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
