3e08ec5c21eaf93f573eaf520c6d7ec72de0cd34
Chris Partridge <root@co3xist.net>
2017-12-02 18:48:26 -0500

    Small fix

--- tools/worker_zone.php
+++ tools/worker_zone.php
@@ -53,7 +53,7 @@ if(file_exists($argv[1])) {
 					$counter++;
 					
 					if($counter > 500) {
-						$dbInsertNewDomain = $mysqli->query($query . implode(',', $queryValues));
+						$dbInsertNewDomain = $mysqli->query($queryBase . implode(',', $queryValues));
 						if(!$dbInsertNewDomain) {
 							echo "There was an error running the insert query. No domain added." . PHP_EOL;
 							echo "SQL error information: " . $mysqli->error . PHP_EOL;
@@ -67,7 +67,7 @@ if(file_exists($argv[1])) {
 	}
 	
 	if(count($queryValues) > 0) {
-		$dbInsertNewDomain = $mysqli->query($query . implode(',', $queryValues));
+		$dbInsertNewDomain = $mysqli->query($queryBase . implode(',', $queryValues));
 		if(!$dbInsertNewDomain) {
 			echo "There was an error running the insert query. No domain added." . PHP_EOL;
 			echo "SQL error information: " . $mysqli->error . PHP_EOL;
