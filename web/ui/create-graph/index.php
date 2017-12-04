1c693eb646e08282185ec8db9f78a2571bb88aca
Chris Partridge <root@co3xist.net>
2017-12-04 03:01:42 -0500

    Wording changes, polish

--- web/ui/create-graph/index.php
+++ web/ui/create-graph/index.php
@@ -73,9 +73,22 @@ include "../_inc/format.php";
 			<!-- /.col-lg-4 -->
 			<div class="col-lg-8">
 				<div class="panel-body">
-					<h3>Help</h3>
+					<h3>Notes</h3>
 					
-					<p>dialogue coming soon</p>
+					<p>"Degrees" specifies how many iterations of the graph generator you'd like to make from the starting domain you enter. This isn't <i>exactly</i> degrees of seperation because of how the graph is structured. Effectively, each iteration of the software takes a list of input domains, finds everything they resolve to, and then everything that also resolves to the resolved values.</p>
+					
+					<p>Here are two (abridged) examples, prefaced by their pseudo-degree:
+					<ol>
+						<li>domain (node) -> IPs (edge) -> domains (nodes)</li>
+						<li>domain -> IPs -> domains -> more IPs -> more domains</li>
+					</ol>
+					</p>
+					
+					<p>You can see how this has the capacity to go exponential - especially as more data is added to the system and more popular links are established. By default, the system resolves two degrees and does not use MX or NS records. This is because MX and NS records are typically quite centralized, consuming a huge amount of processing time only to generate completely unintelligble graphs without any actionable data. In most cases, use of both of these fields should be avoided unless you're sure of what you're doing.</p>
+					
+					<p>To prevent the abuse of this system, MX and NS lookups are disabled for anyone without an API key seeking to perform a >1 degree lookup. Additionally, >2 degree lookups are forbidden for non-keyholders until a system is set up that retrieves and auto-excludes edges that are too populous for relevant results to be fetched. Researchers who need additional access to dnstrace (and seek to avoid some of the listed limits) are again encouraged to contact the maintainer for API keys by email.</p>
+					
+					<p><b>This system is currently in beta and lacks strong performance optimizations, which will be completed subject to maintainer availability.</b></p>
 				</div>
 			</div>
 			<!-- /.col-lg-8 -->
--- web/ui/home/index.php
+++ web/ui/home/index.php
@@ -24,11 +24,13 @@ include "../_inc/format.php";
 						<p>By examining the relationships between domain resolution and (eventually) WHOIS data, we can uncover many secrets hidden in the noise of DNS. dnstrace is a suite of tools that ingest, acquire, parse, and render domain data from many sources on a large scale. It is hosted here and limited access is provided free to all people who wish to perform research using its database.</p>
 						
 						<p>Possible uses include:
+						<ul>
 							<li>Examine domain relationships with force-directed graphs.</li>
 							<li>Find patterns in cybercrime to discern threat actor movements.</li>
 							<li>Detect information leakage due to human errors.</li>
 							<li>Generate reputation for a domain (soon).</li>
 							<li>Create and download probabilistic blacklists (soon).</li>
+						</ul>
 						</p>
 						
 						<p>dnstrace is a tool designed to be used by cybersecurity professionals and researchers. Please use this service responsibly. If you are a researcher and would like additional access to the system (via API key for extended functionality or otherwise), please email the maintainer with details of your usage - we'll get back to you ASAP.</p>
@@ -41,12 +43,23 @@ include "../_inc/format.php";
 			<div class="col-lg-4">
 				<div class="panel panel-default">
 					<div class="panel-heading">
+						Contact
+					</div>
+					<div class="panel-body">
+						<p>This is a project developed and maintained by <a href="https://chris.partridge.tech/" target="_blank">Chris Partridge</a>. This site is made available for research purposes to all people free of charge, and is to my knowledge the first tool of its kind provided for free to the public.</p>
+					</div>
+				</div>
+			</div>
+			<!-- /.col-lg-4 -->
+			<div class="col-lg-4">
+				<div class="panel panel-default">
+					<div class="panel-heading">
 						Credits
 					</div>
 					<div class="panel-body">
-						<p>Project developed and maintained by <a href="https://chris.partridge.tech/" target="_blank">Chris Partridge</a>.</p>
-						<p>Magnificent D3.js wizardry powering graph databases by <a href="https://bl.ocks.org/eyaler" target="_blank">eyaler</a>.</p>
-						<p>Bootstrap template provided by David Miller of <a href="http://blackrockdigital.io/" target="_blank">Blackrock Digital</a>.</p>
+						<p>D3.js wizardry powering graph visualization by <a href="https://bl.ocks.org/eyaler" target="_blank">eyaler</a>.</p>
+						<p>Bootstrap template by <a href="http://davidmiller.io/"  target="_blank">David Miller</a> of <a href="http://blackrockdigital.io/" target="_blank">Blackrock Digital</a>, MIT license.</p>
+						<p>Gorgeous loading icon by <a href="http://ciaccodavi.de/" target="_blank">Ciacco Davide</a>.</p>
 					</div>
 				</div>
 			</div>
