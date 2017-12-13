566b1120cc1740198f5e56d1bc1cc5e90eec9822
Chris Partridge <root@co3xist.net>
2017-12-13 02:52:42 -0500

    Another odd fix?

--- tools/badger_initializer.php
+++ tools/badger_initializer.php
@@ -18,34 +18,32 @@ $ext = new Extract(null, null, Extract::MODE_ALLOW_ICANN);
 $offset = 0;
 
 while(true) {
-	$dbGet = $mysqli->query("SELECT * FROM `BADGER_Jobs` WHERE `Issued` = 0");
+	$dbGet = $mysqli->query("SELECT * FROM `BADGER_Jobs` WHERE `Issued` = 0 LIMIT 1");
 	$fqdns = [];
 	if(mysqli_num_rows($dbGet) == 0) {
 		sleep(2);
 	} else {
-		while($row = $dbGet->fetch_assoc()) {
-			$dbGetDomains = $mysqli->query("SELECT * FROM `Reputation` LIMIT " . $row["Requested"] . " OFFSET " . $offset);
-			
-			$numRet = $dbGetDomains->num_rows;
-			
-			while($aDomain = $dbGetDomains->fetch_assoc()) {
-				$fqdns[] = fixDomain($aDomain["Subdomain"], $aDomain["Domain"]);
-			}
-			
-			$dbGetJobs = $mysqli->query("UPDATE `BADGER_Jobs` SET `IdxStart` = '" . $offset . "', `IdxEnd` = '" . ($offset + $numRet) . "', `Issued` = 1 WHERE `BadgerID` = '" . $row["BadgerID"] . "'");
-			
-			$data = json_encode($fqdns);
-			$dbInsertJob = $mysqli->query("INSERT INTO `BADGER_Temp` (`BadgerID`, `Data`) VALUES ('" . $row["BadgerID"] . "', '" . $data . "')");
-
-			echo "Issued " . $offset . " -> " . ($offset + $numRet) . " to " . $row["BadgerID"] . PHP_EOL;
-			
-			if($numRet < $row["Requested"]) {
-				$offset = 0;
-			} else {
-				$offset = $offset + $numRet;
-			}
+		$row = $dbGet->fetch_assoc();
+		$dbGetDomains = $mysqli->query("SELECT * FROM `Reputation` LIMIT " . $row["Requested"] . " OFFSET " . $offset);
+		
+		$numRet = $dbGetDomains->num_rows;
+		
+		while($aDomain = $dbGetDomains->fetch_assoc()) {
+			$fqdns[] = fixDomain($aDomain["Subdomain"], $aDomain["Domain"]);
 		}
-		sleep(1);
+		
+		$dbGetJobs = $mysqli->query("UPDATE `BADGER_Jobs` SET `IdxStart` = '" . $offset . "', `IdxEnd` = '" . ($offset + $numRet) . "', `Issued` = 1 WHERE `BadgerID` = '" . $row["BadgerID"] . "'");
+		
+		$data = json_encode($fqdns);
+		$dbInsertJob = $mysqli->query("INSERT INTO `BADGER_Temp` (`BadgerID`, `Data`) VALUES ('" . $row["BadgerID"] . "', '" . $data . "')");
+
+		echo "Issued " . $offset . " -> " . ($offset + $numRet) . " to " . $row["BadgerID"] . PHP_EOL;
+		
+		if($numRet < $row["Requested"]) {
+			$offset = 0;
+		} else {
+			$offset = $offset + $numRet;
+		}
 	}
 }
 ?>
