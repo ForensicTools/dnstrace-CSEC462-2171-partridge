a871a1c0bfe32351151714a685925bac0bea47e9
Chris Partridge <root@co3xist.net>
2017-12-14 00:21:42 -0500

    Fixed badger_submit

--- web/api/badger_submit.php
+++ web/api/badger_submit.php
@@ -9,11 +9,23 @@ use LayerShifter\TLDExtract\Extract;
 
 $ext = new Extract(null, null, Extract::MODE_ALLOW_ICANN);
 
-if(array_key_exists("key", $_POST) && array_key_exists("fqdn", $_POST) && array_key_exists("type", $_POST) && array_key_exists("result", $_POST)) {
-	$key = mysqli_real_escape_string($mysqli, htmlspecialchars($_POST["key"]));
-	$postedFQDN = mysqli_real_escape_string($mysqli, htmlspecialchars($_POST["fqdn"]));
-	$postedType = mysqli_real_escape_string($mysqli, htmlspecialchars($_POST["type"]));
-	$postedResult = mysqli_real_escape_string($mysqli, $_POST["result"]);
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$post = json_decode(file_get_contents("php://input"), true);
+} else {
+	echo json_encode(array("Success" => false, "Reason" => "You need to POST to this endpoint."));
+	include "inc/exit.php";
+}
+
+if($post === null) {
+	echo json_encode(array("Success" => false, "Reason" => "Error decoding JSON serverside."));
+	include "inc/exit.php";
+}
+
+if(array_key_exists("key", $post) && array_key_exists("fqdn", $post) && array_key_exists("type", $post) && array_key_exists("result", $post)) {
+	$key = mysqli_real_escape_string($mysqli, $post["key"]);
+	$postedFQDN = mysqli_real_escape_string($mysqli, $post["fqdn"]);
+	$postedType = mysqli_real_escape_string($mysqli, $post["type"]);
+	$postedResult = $post["result"];
 } else {
 	echo json_encode(array("Success" => false, "Reason" => "Missing critical field of data."));
 	include "inc/exit.php";
@@ -26,14 +38,7 @@ if(mysqli_num_rows($dbGet) == 0) {
 	include "inc/exit.php";
 }
 
-$json = json_decode($postedResult, true);
-	
-if(!$json) {
-	echo json_encode(array("Success" => false, "Reason" => "Error decoding data. Please contact the administrator."));
-	include "inc/exit.php";
-}	
-
-$dbInsertJob = $mysqli->query("INSERT INTO `BADGER_Validate` (`Key`, `FQDN`, `Type`, `Data`) VALUES ('" . $key . "', '" . $postedFQDN . "', '" . $postedType . "', '" . $postedResult . "')");
+$dbInsertJob = $mysqli->query("INSERT INTO `BADGER_Validate` (`BadgerID`, `FQDN`, `Type`, `Data`) VALUES ('" . $key . "', '" . $postedFQDN . "', '" . $postedType . "', '" . json_encode($postedResult) . "')");
 
 if(!$dbInsertJob) {
 	echo json_encode(array("Success" => false, "Reason" => "Error saving data. Please contact the administrator."));
